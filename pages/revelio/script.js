a1ad33c3a527e49a98c6a73cb6088e9b90c6824c
Erik Carvalho <erik.carvalho94@hotmail.com>
2024-01-07 22:34:13 -0300

    Refiz todo o código

--- pages/revelio/script.js
+++ pages/revelio/script.js
@@ -1,3 +1,43 @@
+const slide = (direction) => {
+    const carouselBox = document.querySelector('.carouselbox');
+    const movies = carouselBox.querySelectorAll('img');
+    const movieWidth = movies[0].clientWidth + 10;
+
+    const scrollPos = carouselBox.scrollLeft;
+    const visibleWidth = carouselBox.clientWidth;
+    const totalWidth = carouselBox.scrollWidth;
+
+    const fullyVisible = Math.floor(visibleWidth / movieWidth) * movieWidth;
+    const partiallyVisible = visibleWidth % movieWidth;
+    const threshold = totalWidth - fullyVisible;
+
+    let newPosition = 0;
+
+    if (direction === 'left') {
+        newPosition = scrollPos - fullyVisible;
+        if (newPosition < 0) {
+            newPosition = 0;
+        }
+
+    } else if (direction === 'right') {
+        newPosition = scrollPos + fullyVisible;
+        if (newPosition > threshold) {
+            newPosition = threshold;
+        }
+    }
+
+    carouselBox.scrollTo({
+        left: newPosition,
+        behavior: 'smooth'
+    });
+};
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
+
 // Função para voltar ao topo da página
 function voltarAoTopo() {
     if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
