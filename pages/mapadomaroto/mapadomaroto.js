91e53b548eebd87b490cefe178ad742549eac244
Bárbara Leão <barbaraleaobsc@gmail.com>
2024-01-24 16:58:17 -0300

    teste esconder mapa do maroto e infos

--- pages/mapadomaroto/mapadomaroto.js
+++ pages/mapadomaroto/mapadomaroto.js
@@ -1,1 +1,10 @@
 alert('O Sr. Aluado apresenta seus cumprimentos ao Prof. Snape e pede que ele não meta seu nariz anormalmente grande no que não é de sua conta.')
+
+document.addEventListener('DOMContentLoaded', function() {
+    var botaoToggle = document.getElementById('botaoToggle');
+    var informacoes = document.getElementById('informacoes');
+  
+    botaoToggle.addEventListener('click', function() {
+      informacoes.classList.toggle('esconder');
+    });
+  });
