042ef06393391ebbcf482593c75ad08edefab781
Bárbara Leão <barbaraleaobsc@gmail.com>
2024-01-24 13:56:26 -0300

    mudança no alert mapa do maroto

--- pages/mapadomaroto/mapadomaroto.js
+++ pages/mapadomaroto/mapadomaroto.js
@@ -1,1 +1,1 @@
-alert('Bem vindos ao Mapa do Maroto!')
+alert('O Sr. Aluado apresenta seus cumprimentos ao Prof. Snape e pede que ele não meta seu nariz anormalmente grande no que não é de sua conta.')
