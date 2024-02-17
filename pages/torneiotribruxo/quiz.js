1ea108e8b8065ff2cd5009e8aa361ee79d8049e3
Christian Matias <cmatiasa@gmail.com>
2024-02-17 15:16:09 -0300

    mudanca do quiz

---
+++ pages/torneiotribruxo/quiz.js
@@ -0,0 +1,36 @@
+export default [
+    {
+      question: "Quem é o diretor de Hoghwarts?",
+      answers: [
+        { option: "Severus Snape", correct: false },
+        { option: "Rúbeo Hagrid", correct: false },
+        { option: "Minerva Mcgonagall", correct: false },
+        { option: "Albus Dumbledore", correct: true },
+        { option: "Dolores Umbridge", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 5 x 2?",
+      answers: [
+        { option: "9", correct: false },
+        { option: "10", correct: true },
+        { option: "11", correct: false },
+      ],
+    },
+    {
+      question: "Quanto é 10 x 8?",
+      answers: [
+        { option: "800", correct: false },
+        { option: "90", correct: false },
+        { option: "80", correct: true },
+      ],
+    },
+    {
+      question: "Quanto é 7 x 7?",
+      answers: [
+        { option: "70", correct: false },
+        { option: "56", correct: false },
+        { option: "49", correct: true },
+      ],
+    },
+  ];
