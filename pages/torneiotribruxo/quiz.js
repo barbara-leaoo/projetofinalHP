f25aa5439b072656b5f663c46a9b120e16c87dd0
Christian Matias <cmatiasa@gmail.com>
2024-02-18 21:44:08 -0300

    Confeccao do quiz

--- pages/torneiotribruxo/quiz.js
+++ pages/torneiotribruxo/quiz.js
@@ -1,36 +1,101 @@
 export default [
     {
-      question: "Quem é o diretor de Hoghwarts?",
+      question: "Qual é o nome completo do personagem principal da série Harry Potter?",
       answers: [
-        { option: "Severus Snape", correct: false },
-        { option: "Rúbeo Hagrid", correct: false },
-        { option: "Minerva Mcgonagall", correct: false },
-        { option: "Albus Dumbledore", correct: true },
-        { option: "Dolores Umbridge", correct: false },
+        { option: "Harry Severos Potter", correct: false },
+        { option: "Harry Ronald Potter", correct: false },
+        { option: "Harry Thiago Potter", correct: false },
+        { option: "Harry James Potter", correct: true },
+        { option: "Harry Umbridge Potter", correct: false },
       ],
     },
     {
-      question: "Quanto é 5 x 2?",
+        question: "Quem é o diretor de Hoghwarts?",
+        answers: [
+          { option: "Severo Snape", correct: false },
+          { option: "Rúbeo Hagrid", correct: false },
+          { option: "Minerva Mcgonagall", correct: false },
+          { option: "Alvo Dumbledore", correct: true },
+          { option: "Dolores Umbridge", correct: false },
+        ],
+    },
+    {
+      question: "Quem é o melhor amigo de Harry Potter em Hogwarts?",
       answers: [
-        { option: "9", correct: false },
-        { option: "10", correct: true },
-        { option: "11", correct: false },
+        { option: "Rony Weasley", correct: true },
+        { option: "Draco Malfoy", correct: false },
+        { option: "Neville Longbottom", correct: false },
+        { option: "Hermione Granger", correct: false },
+        { option: "Cedrico Diggory", correct: false },
       ],
     },
     {
-      question: "Quanto é 10 x 8?",
+      question: "Qual é o nome do animal de estimação de Harry Potter?",
       answers: [
-        { option: "800", correct: false },
-        { option: "90", correct: false },
-        { option: "80", correct: true },
+        { option: "Perebas", correct: false },
+        { option: "Edwiges", correct: true },
+        { option: "Bicuço", correct: false },
+        { option: "Madame Nora", correct: false },
+        { option: "Naguini", correct: false },
       ],
     },
     {
-      question: "Quanto é 7 x 7?",
-      answers: [
-        { option: "70", correct: false },
-        { option: "56", correct: false },
-        { option: "49", correct: true },
-      ],
-    },
+        question: "Quem é o principal vilão na série Harry Potter?",
+        answers: [
+          { option: "Serevo Snape", correct: false },
+          { option: "Dolores Umbridge", correct: false },
+          { option: "Barto Crouch Jr. ", correct: false },
+          { option: "Sirius Black", correct: false },
+          { option: "Lord Voldemort", correct: false },
+        ],
+      },
+      {
+        question: "Qual é o nome do professor de Poções de Harry Potter em Hogwarts?",
+        answers: [
+          { option: "Serevo Snape", correct: true },
+          { option: "Gilderoy Lockheart", correct: false },
+          { option: "Sibila Trelawney", correct: false },
+          { option: "Pomona Sprout", correct: false },
+          { option: "Rolanda Hooch", correct: false },
+        ],
+      },
+      {
+        question: "Qual é a casa em Hogwarts a que pertence Harry Potter?",
+        answers: [
+          { option: "Sonserina", correct: true },
+          { option: "Grifinória", correct: false },
+          { option: "Corvinal", correct: false },
+          { option: "Lufa-Lufa", correct: false },
+        ],
+      },
+      {
+        question: "Qual é o nome da escola rival de Hogwarts?",
+        answers: [
+          { option: "Instituto Durmstrang", correct: true },
+          { option: "Castelobruxo", correct: false },
+          { option: "Academia de Magia Beauxbatons", correct: false },
+          { option: "Mahoutokoro", correct: false },
+          { option: "Koldovstoretz", correct: false },
+        ],
+      },
+      {
+        question: "Qual é o nome da professora que ensina Transfiguração?",
+        answers: [
+          { option: "Guilhermina Grubbly-Plank", correct: false },
+          { option: "Sibila Trelawney", correct: false },
+          { option: "Minerva McGonagall", correct: true },
+          { option: "Dolores Umbridge", correct: false },
+          { option: "Rolanda Hooch", correct: false },
+        ],
+      },
+      {
+        question: "Quem é o guardião das chaves e terrenos de Hogwarts?",
+        answers: [
+          { option: "Argo Filch", correct: false },
+          { option: "Alvo Dumbledore", correct: false },
+          { option: "Rúbeo Hagrid", correct: true },
+          { option: "Dobby", correct: false },
+          { option: "Severo Snape", correct: false },
+        ],
+      },
   ];
